0b5cf41b5d5c850282ff6ac4bae08873bf8f2eca
anaisabelveg <anaisabelveg@gmail.com>
2023-10-24 16:53:01 +0200

    instancia alumno

--- src/com/softtek/AppMain.java
+++ src/com/softtek/AppMain.java
@@ -1,10 +1,16 @@
 package com.softtek;
 
+import com.softtek.models.Alumno;
+import com.softtek.models.Direccion;
+
 public class AppMain {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Bienvenidos al curso de GIT");
+		
+		Alumno alumno = new Alumno(1, "Pepito", 6.3, new Direccion("Mayor", 5, "Madrid"));
+		System.out.println(alumno);
 
 	}
 
